6d1d52d64ae19aa434c85f535dcffb07ba06919e
Mete A <meteaydin050@gmail.com>
2022-07-23 12:29:36 +0300

    Heroku bug fix.

--- src/main/java/tr/com/metea/hotelium/HoteliumApplication.java
+++ src/main/java/tr/com/metea/hotelium/HoteliumApplication.java
@@ -2,11 +2,12 @@ package tr.com.metea.hotelium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class HoteliumApplication {
 
 	@PostConstruct
