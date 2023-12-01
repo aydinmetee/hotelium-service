0fca794618f2e6983d70afa6aa882e1ec3c74805
meteaydin <mete.aydin@eveshop.com.tr>
2023-12-02 00:58:59 +0300

    added configs

--- src/main/java/tr/com/metea/hotelium/config/SpringSecurityConfig.java
+++ src/main/java/tr/com/metea/hotelium/config/SpringSecurityConfig.java
@@ -35,19 +35,28 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v3
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            // other public endpoints can be appended to this array
+            "/api/v1/auth/**",
+            "/api-docs/**",
+            "/api-docs",
             // -- Swagger UI v2
             "/v2/api-docs",
             "/swagger-resources",
             "/swagger-resources/**",
             "/configuration/ui",
             "/configuration/security",
-            "/swagger-ui.html",
             "/webjars/**",
             // other public endpoints of your API may be appended to this array
             "/auth/login",
             "/auth/register",
             "/org-def",
-            "/combo/orgs"
+            "/combo/orgs",
+            "/**"
     };
 
     @Override
--- src/main/java/tr/com/metea/hotelium/config/SwaggerConfig.java
+++ src/main/java/tr/com/metea/hotelium/config/SwaggerConfig.java
@@ -1,10 +1,7 @@
 package tr.com.metea.hotelium.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -13,13 +10,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Eve Template", version = "v1"))
-@SecurityScheme(
-		name = "bearerAuth",
-		type = SecuritySchemeType.HTTP,
-		bearerFormat = "JWT",
-		scheme = "bearer",
-		in = SecuritySchemeIn.HEADER
-)
 public class SwaggerConfig {
 
 }
